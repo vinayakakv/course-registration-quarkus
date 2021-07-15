dd31c5b28a77932875769085a1140af3a1543995
Vinayaka K V <me.vinayakakv@gmail.com>
2021-07-15 10:39:20 +0530

    Initial commit 🎉

---
+++ src/test/java/org/viyk/NativeCourseRegistrationIT.java
@@ -0,0 +1,9 @@
+package org.viyk;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCourseRegistrationIT extends CourseRegistrationTest {
+
+    // Execute the same tests but in native mode.
+}
