c4ed48ca0c9f1ccdfe2b1dcced61032549e36c5c
Vinayaka K V <me.vinayakakv@gmail.com>
2021-07-15 18:48:56 +0530

    Implement CourseRegistration

--- src/main/java/org/viyk/CourseResource.java
+++ src/main/java/org/viyk/CourseResource.java
@@ -4,8 +4,14 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotBlank;
 import javax.ws.rs.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 @Path("/course")
 public interface CourseResource extends PanacheEntityResource<Course, String> {
@@ -15,5 +21,21 @@ public interface CourseResource extends PanacheEntityResource<Course, String> {
 class Course extends PanacheEntityBase {
     @Id
     public String id;
+    @NotBlank
     public String name;
+
+    @ManyToMany(mappedBy = "courses", fetch = FetchType.EAGER)
+    protected Set<Student> students = new HashSet<>();
+
+    @Transactional
+    public void addStudent(Student student) {
+        students.add(student);
+        this.persist();
+    }
+
+    @Transactional
+    public void removeStudent(Student student){
+        students.removeIf(x-> x.id.equals(student.id));
+        this.persist();
+    }
 }
--- src/main/java/org/viyk/StudentResource.java
+++ src/main/java/org/viyk/StudentResource.java
@@ -3,10 +3,12 @@ package org.viyk;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.transaction.Transactional;
 import javax.validation.constraints.NotBlank;
 import javax.ws.rs.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 @Path("/student")
 public interface StudentResource extends PanacheEntityResource<Student, String> {
@@ -20,4 +22,22 @@ class Student extends PanacheEntityBase {
     public String name;
     @NotBlank
     public String email;
+
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinTable(name = "Enrollment",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    protected Set<Course> courses = new HashSet<>();
+
+    @Transactional
+    public void enroll(Course course) {
+        courses.add(course);
+        this.persist();
+    }
+
+    @Transactional
+    public void delete_enroll(Course course) {
+        courses.removeIf(x-> x.id.equals(course.id));
+        this.persist();
+    }
 }
